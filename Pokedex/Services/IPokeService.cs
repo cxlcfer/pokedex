3798c50b6cf0b86a77bc7dc1b7df234a42cf118a
cxlcfer <geovanahscudeletti@gmail.com>
2023-03-14 11:41:42 -0300

    Adicionado as coisas do pdf

--- Pokedex/Services/IPokeService.cs
+++ Pokedex/Services/IPokeService.cs
@@ -1,9 +1,13 @@
 using Pokedex.Models;
 namespace Pokedex.Services;
 
-    public interface IPokeService
-    {
-        Pokemon GetPokemon(int Numero);
-        List<Pokemon> GetPokemons();
-        List<Tipo> GetTipos();
-    }
+public interface IPokeService
+{
+    Pokemon GetPokemon(int Numero);
+    List<Pokemon> GetPokemons();
+    List<Tipo> GetTipos();
+
+    PokedexDto GetPokedexDto();
+    DetailsDto GetDetailedPokemon(int Numero);
+    Tipo GetTipo(string Nome);
+}
